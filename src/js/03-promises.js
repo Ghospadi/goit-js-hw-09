c20b7bebe822acf3ba617915bb4a2c002354e2d2
ghospadi <Lolr2223@gmail.com>
2022-06-18 16:12:51 +0300

    finish task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const onClick = e => {
+  e.preventDefault();
+
+  console.log(e);
+
+  const { delay, step, amount } = e.currentTarget;
+
+  let firstDelay = +delay.value;
+  let stepDelay = +step.value;
+  let amountDelays = +amount.value;
+
+  for (let startPosition = 1; startPosition <= amountDelays; startPosition++) {
+    createPromise(startPosition, firstDelay);
+    firstDelay += stepDelay;
+  }
+};
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise.then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  });
+
+  promise.catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
+form.addEventListener('submit', onClick);
